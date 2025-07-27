74ab084b7da4ec2df1d9aba7a2956bf71d3b7043
rahmimuaz <rahmimuaz10@gmail.com>
2025-07-27 16:54:35 +0530

    hgh
    asdfasdf

--- backend/server.js
+++ backend/server.js
@@ -126,5 +126,5 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
